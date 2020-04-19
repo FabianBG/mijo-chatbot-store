54993d74e5c4217425919e6de81bbded3d4a3f4e
FabianBG <f4b4g3@gmail.com>
2020-04-19 17:52:48 -0500

    Services to add and remove products

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { exec } = require("child_process");
 const slugify = require("slugify");
 const validateBody = require("./bodyValidator");
+const fs = require("fs");
 
 var router = express.Router();
 
@@ -37,50 +38,93 @@ router.post("/store/build-site", function(req, res) {
     res.status(500).send(errors);
   }
   console.log(`${new Date()} [INFO] BUILD INIT SITE-ID=${req.id}`);
-  exec(`./scripts/build-site ${req.body.phone}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`${new Date()} [ERROR] build-site: ${error}`);
-      return;
-    }
-    console.log(`${new Date()} [INFO] BUILD END OUTPUT`, stdout, stderr);
-
-    res.status(200).send({ updated: true });
-  });
-});
-
-router.post("/store/add-product", function(req, res) {
-  console.log(`${new Date()} [INFO] BUILD END OUTPUT`, stdout, stderr);
   exec(
-    `cd sites/${req.body.id}; npx gatsby build --prefix-paths; cp public/* ../../public/${req.body.id}/`,
+    `./scripts/build-site-cache ${req.body.phone}`,
     (error, stdout, stderr) => {
       if (error) {
-        console.error(`exec error: ${error}`);
+        console.error(`${new Date()} [ERROR] build-site: ${error}`);
         return;
       }
       console.log(`${new Date()} [INFO] BUILD END OUTPUT`, stdout, stderr);
-      res.send({ ok: true });
+
+      res.status(200).send({ updated: true });
     }
   );
+});
+
+router.post("/store/add-product", function(req, res) {
+  const errors = validateBody(req.body, {
+    phone: (value) => (!value ? "phone is required" : false),
+    name: (value) => (!value ? "name is required" : false),
+    price: (value) => (!value ? "price is required" : false),
+  });
+
+  if (Object.keys(errors).length > 0) {
+    res.status(500).send(errors);
+  }
+
+  const { name, phone, price } = req.body;
+  const productSlug = slugify(name.toLowerCase());
+  const dataFolder = `sites/${phone}/src/data/products`;
+
+  fs.readdir(dataFolder, function(err, items) {
+    let nextID = items.length;
+    for (let i = 0; i < items.length; i++) {
+      const id = parseInt(items[i].split(".")[0]);
+      if (i !== id) {
+        nextID = i;
+        break;
+      }
+    }
+    // TODO: manage images from chatbot
+    const image = "default.png";
+    const content = `
+    {
+      "id": "MJPR-${nextID}",
+      "name": "${name}",
+      "price": "${price}",
+      "description": "",
+      "currency": "$",
+      "image": "${image}"
+    }
+    `;
+    fs.writeFileSync(
+      `${dataFolder}/${nextID}.${productSlug}.json`,
+      content,
+      "UTF-8"
+    );
+    res.status(200).send(content);
+  });
 });
 
 router.post("/store/delete-product", function(req, res) {
-  exec(
-    `cd sites/${req.body.id}; npx gatsby build --prefix-paths; cp public/* ../../public/${req.body.id}/`,
-    (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
+  const errors = validateBody(req.body, {
+    phone: (value) => (!value ? "phone is required" : false),
+    id: (value) => (!value ? "id is required" : false),
+  });
+
+  if (Object.keys(errors).length > 0) {
+    res.status(500).send(errors);
+  }
+
+  const { id, phone } = req.body;
+  const dataFolder = `sites/${phone}/src/data/products`;
+
+  fs.readdir(dataFolder, function(err, items) {
+    for (let i = 0; i < items.length; i++) {
+      const fileID = items[i].split(".")[0];
+      if (id === fileID) {
+        fs.unlinkSync(`${dataFolder}/${items[i]}`);
+        break;
       }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
-      res.send({ ok: true });
     }
-  );
+    res.status(200).send({ deleted: true });
+  });
 });
 
 router.post("/store/process-order", function(req, res) {
-  console.log(req);
-  res.status(200).send({ ok: true });
+  console.log(req.body);
+  res.status(200).send(req.body);
 });
 
 module.exports = router;
--- sites-config.js
+++ sites-config.js
@@ -1,3 +1,3 @@
 export default {
-  API_PLACE_ORDER_URL: "http://localhost:3000/mijo/store/order",
+  API_PLACE_ORDER_URL: "http://localhost:3000/mijo/_api/store/process-order",
 };
